1d7f38decda8d819d35f6183a7b78f878b4f1d1d
Carol Johnson <cjohns83@uwo.ca>
2019-12-23 12:04:58 -0500

    Update TestTargetItem module
    Relocate to general testing directory
    Update to run from 'Reload and Test' button in developer mode

--- ImageQuizzer/Testing/Python/TestTargetItem.py
+++ ImageQuizzer/Testing/Python/TestTargetItem.py
@@ -1,104 +1,85 @@
-import os
-import unittest
 import vtk, qt, ctk, slicer
 from slicer.ScriptedLoadableModule import *
-import logging
 from TargetItem import *
-import urllib.request # import submodule directly
+from TestingStatus import *
 
+import os
 import sys
-import warnings
-import numpy as np
-from pathlib import Path
-import imp
 
 
-
+##########################################################################
 #
-# TestQuestion
+# TestTargetItem
 #
+##########################################################################
 
 class TestTargetItem(ScriptedLoadableModule):
     """Uses ScriptedLoadableModule base class, available at:
     https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
     """
 
+    #------------------------------------------- 
+
     def __init__(self, parent):
         ScriptedLoadableModule.__init__(self, parent)
-        self.parent.title = "Test TargetItem" # TODO make this more human readable by adding spaces
+        self.parent.title = "Test Target Item" 
         self.parent.categories = ["Testing.ImageQuizzer"]
         self.parent.dependencies = []
-        self.parent.contributors = ["Carol Johnson (Baines Imaging Research Laboratory)"] 
+        self.parent.contributors = ["Carol Johnson (Baines Imaging Research Laboratories)"] 
         self.parent.helpText = """
-        This is an example of scripted loadable module bundled in an extension.
-        It performs a simple thresholding on the input volume and optionally captures a screenshot.
+        This tests building a session - reading in instructions how the session will be run 
+        through an xml file.
         """
         self.parent.helpText += self.getDefaultModuleDocumentationLink()
         self.parent.acknowledgementText = """
-        This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
-        and Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
-        """ # replace with organization, grant and thanks.
+        This file was originally developed by Carol Johnson of the Baines Imaging Research Laboratories, 
+        under the supervision of Dr. Aaron Ward
+        """ 
 
+
+##########################################################################
 #
-# TestTargetItemWidget
+# TestQuestionSet_ModuleWidget
 #
+##########################################################################
 
 class TestTargetItemWidget(ScriptedLoadableModuleWidget):
     """Uses ScriptedLoadableModuleWidget base class, available at:
     https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
     """
 
+    #------------------------------------------- 
     def setup(self):
+        self.developerMode = True
         ScriptedLoadableModuleWidget.setup(self)
-        # Instantiate and connect widgets ...
-        
-        
-        # Start test button
-        startTestButton = qt.QPushButton("Start Tests TargetItem Class")
-        startTestButton.toolTip = "start unit tests for TargetItem class."
-#         self.testFormLayout.addWidget(startTestButton)
-        self.layout.addWidget(startTestButton)
-        startTestButton.connect('clicked(bool)', self.onStartTestButtonClicked)
 
-        # Collapsible button
-        testTargetItemCollapsibleButton = ctk.ctkCollapsibleButton()
-        testTargetItemCollapsibleButton.text = "Testing TargetItem Display"
-        self.layout.addWidget(testTargetItemCollapsibleButton)
-        
-            
-        # Layout within the collapsible buttons
-        self.groupsLayout = qt.QFormLayout(testTargetItemCollapsibleButton)
 
-        # Add vertical spacer
-        self.layout.addStretch(1)
-        
-        # Set local var as instance attribute
-        self.startTestButton = startTestButton
+##########################################################################
+#
+# TestTargetItem_ModuleLogic
+#
+##########################################################################
 
-        # reload class being tested
-        #    This reloads the scripts for testing which are not handled by the 'Reload' button
-        self.reloadClassForTest()
-        
-    def reloadClassForTest(self):
-        # TODO: Fix so that you don't have to 'Reload' twice for this to take effect
+class TestTargetItemLogic(ScriptedLoadableModuleLogic):
+    """This class should implement all the actual
+    computation done by your module.  The interface
+    should be such that other python code can import
+    this class and make use of the functionality without
+    requiring an instance of the Widget
+    """
 
-        # function to force reload of scripts that are not 'loadable' in Slicer
-        sModule = "TargetItem"
-        sScriptName = sModule + '.py'
-        # Test files are located in .\Testing\Python\TestModule\xxx.py - i.e. up 4 levels to root
-        sProjectPath = Path(__file__).parent.parent.parent.parent
-        print ('**** : ' + str(sProjectPath))
-        sSourceFile = str(sProjectPath) + '\\' + sScriptName
-        fp = open(sSourceFile, "r")
-        globals()[sModule] = imp.load_module(sModule, fp, sSourceFile, ('.py', 'r', imp.PY_SOURCE))
-        fp.close()
+    def __init__(self):
+        ScriptedLoadableModuleLogic.__init__(self)
+        self.sClassName = type(self).__name__
+        print("\n************ Unittesting for class Target Item ************\n")
+        self.sessionTestStatus = TestingStatus()
 
-    def onStartTestButtonClicked(self):
-        print("\n************ Unittesting for class Page ************\n")
-        oTestTargetItem = TestTargetItemTest()
-        oTestTargetItem.runTest(self.groupsLayout)
-        print("\n************ Test Complete ************\n")
 
+##########################################################################
+#
+# TestTargetItem_ModuleTest
+#
+##########################################################################
 
 class TestTargetItemTest(ScriptedLoadableModuleTest):
     """
@@ -106,6 +87,8 @@ class TestTargetItemTest(ScriptedLoadableModuleTest):
     Uses ScriptedLoadableModuleTest base class, available at:
     https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
     """
+
+    #------------------------------------------- 
 
     def setUp(self):
         """ Do whatever is needed to reset the state - typically a scene clear will be enough.
@@ -119,55 +102,29 @@ class TestTargetItemTest(ScriptedLoadableModuleTest):
         scriptedModulesPath = os.path.dirname(scriptedModulesPath)
         self.testDataPath = os.path.join(scriptedModulesPath, 'Testing', 'TestData')
 
-        
+       
+    #------------------------------------------- 
 
-    def DisplayTestResults(self, tupResults):
-        # print the boolean results for each test
-        self.tupResults = tupResults
-        print('--- Test Results ' + ' --------- ' + self.sClassName + ' functions -----')
-        for i in range(len(tupResults)):
-            success = False   # assume fail
-            sFname, success = tupResults[i]
-            if success == True:
-                sDisplay = "Ahh....... test passed.    : "
-            else:
-                sDisplay = "!$!*^&#!@$%! Test Failed!! : "
-            print(sDisplay, i+1, sFname)
-            
-    def AddWidgetToTestFormLayout(self, grpBoxWidget, testFormLayout):
-        # display the resulting widget on the Test layout for visual test
-        self.testFormLayout = testFormLayout
-        self.gbWidget = grpBoxWidget
-        self.testFormLayout.addWidget(self.gbWidget)
-        
-
-    
-    ##########################################################################################
-    #                                TEST FLOW
-    ##########################################################################################
-    
-    def runTest(self, groupsLayout ):
+    def runTest(self ):
         """TODO: for this function to be automatically started with the 
             'Reload and Test' button in Slicer, there cannot be an extra argument here.
             I have the argument 'layout' to be able to display widgets as part of my testing. 
         """
         self.setUp()
+        logic = TestTargetItemLogic()
+
         tupResults = []
-#         self.testFormLayout = layout
-#        slicer.mrmlScene.Clear(0)
         tupResults.append(self.test_NoErrors_LoadNrrd())
         tupResults.append(self.test_NoErrors_LoadNii())
         tupResults.append(self.test_NoErrors_LoadMhd())
         tupResults.append(self.test_NoErrors_LoadSegmentation())
         tupResults.append(self.test_Errors_NonExistentFile())
-
         
-        self.groupsLayout = groupsLayout
-        self.DisplayTestResults(tupResults)
+        
+        logic.sessionTestStatus.DisplayTestResults(tupResults)
  
-    ##########################################################################################
-    #                                    TESTS
-    ##########################################################################################
+
+    #------------------------------------------- 
 
     def test_NoErrors_LoadNrrd(self):
         bTestResult = False
@@ -177,8 +134,9 @@ class TestTargetItemTest(ScriptedLoadableModuleTest):
 #         impath = 'D:\\Users\\cjohnson\\Work\\Resources\\ShareableData\\RegistrationDemos\\FromSlicer\\NRR-CTgLiverAblation\\CTp.nrrd'
 #         strpath = urllib.request.unquote(impath)
 
+        sTestDataSubfolder = 'Brain' 
         sInputFileName = 'ManualRegistrationExample_fixed'
-        strpath = os.path.join(self.testDataPath, '%s.nrrd' %sInputFileName)
+        strpath = os.path.join(self.testDataPath, sTestDataSubfolder, '%s.nrrd' % sInputFileName)
         dictProperties = {}
         
         self.oTarget = DataVolume()
@@ -193,7 +151,7 @@ class TestTargetItemTest(ScriptedLoadableModuleTest):
         tupResult = self.fnName, bTestResult
         return tupResult
 
-    #-----------------------
+    #------------------------------------------- 
 
     def test_NoErrors_LoadNii(self):
         bTestResult = False
@@ -204,8 +162,9 @@ class TestTargetItemTest(ScriptedLoadableModuleTest):
 #         strpath = urllib.request.unquote(impath)
 
         
+        sTestDataSubfolder = 'Brain' 
         sInputFileName = 'ManualRegistrationExample_moving'
-        strpath = os.path.join(self.testDataPath, '%s.nii' %sInputFileName)
+        strpath = os.path.join(self.testDataPath, sTestDataSubfolder, '%s.nii' %sInputFileName)
         dictProperties = {}
 
         self.oTarget = DataVolume()
@@ -220,15 +179,16 @@ class TestTargetItemTest(ScriptedLoadableModuleTest):
         tupResult = self.fnName, bTestResult
         return tupResult
 
-    #-----------------------
+    #------------------------------------------- 
     
     def test_NoErrors_LoadMhd(self):
         
         bTestResult = False
         self.fnName = sys._getframe().f_code.co_name
 
+        sTestDataSubfolder = 'Prostate' 
         sInputFileName = 'Case00'
-        strpath = os.path.join(self.testDataPath, '%s.mhd' %sInputFileName)
+        strpath = os.path.join(self.testDataPath, sTestDataSubfolder, '%s.mhd' %sInputFileName)
         dictProperties = {}
         
         self.oTarget = DataVolume()
@@ -243,14 +203,15 @@ class TestTargetItemTest(ScriptedLoadableModuleTest):
         tupResult = self.fnName, bTestResult
         return tupResult
 
-    #-----------------------
+    #------------------------------------------- 
     
     def test_NoErrors_LoadSegmentation(self):
         bTestResult = False
         self.fnName = sys._getframe().f_code.co_name
 
+        sTestDataSubfolder = 'Prostate' 
         sInputFileName = 'Case00_segmentation'
-        strpath = os.path.join(self.testDataPath, '%s.mhd' %sInputFileName)
+        strpath = os.path.join(self.testDataPath, sTestDataSubfolder, '%s.mhd' %sInputFileName)
         dictProperties = {"labelmap" : True}
         
         self.oTarget = DataVolume()
@@ -265,7 +226,7 @@ class TestTargetItemTest(ScriptedLoadableModuleTest):
         tupResult = self.fnName, bTestResult
         return tupResult
 
-    #-----------------------
+    #------------------------------------------- 
     
     def test_Errors_NonExistentFile(self):
         bTestResult = False
@@ -283,3 +244,21 @@ class TestTargetItemTest(ScriptedLoadableModuleTest):
 
         tupResult = self.fnName, bTestResult
         return tupResult
+
+
+##########################################################################################
+#                      Launching from main (Reload and Test button)
+##########################################################################################
+
+def main(self):
+    try:
+        logic = TestTargetItemLogic()
+        logic.runTest()
+        
+    except Exception as e:
+        print(e)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
