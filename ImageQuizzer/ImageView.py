8e186eff10a3f2cdf4ea9675e9ff9a791206b38a
Carol Johnson <cjohns83@uwo.ca>
2020-02-27 11:57:35 -0500

    ROI Visibility Code
    Turn on/off ROI's in the Subject Hierarchy

--- ImageQuizzer/ImageView.py
+++ ImageQuizzer/ImageView.py
@@ -56,8 +56,10 @@ class ImageView:
             for i in range(len(self.ltupViewNodes)):
                 
                 # get node details
-                slNode, sImageDestination, sOrientation, sViewLayer = self.ltupViewNodes[i]
+                slNode, sImageDestination, sOrientation, sViewLayer, sImageType, sSeriesInstanceUID, sRoiVisibilityCode, lsRoiList = self.ltupViewNodes[i]
                 self.AssignNodesToView(slNode.GetName(), sImageDestination, sViewLayer, sOrientation)
+                if not (sRoiVisibilityCode == 'Empty'):
+                    self.SetRoiVisibility( sSeriesInstanceUID, sRoiVisibilityCode, lsRoiList)
 
 
     #-----------------------------------------------
@@ -67,55 +69,73 @@ class ImageView:
     def BuildViewNodes(self, xImages):
         
         # for each image
-        for i in range(len(xImages)):
+        for indImage in range(len(xImages)):
 
 
             # Extract image attributes
-            sVolumeFormat = self.oIOXml.GetValueOfNodeAttribute(xImages[i], 'format')
-            sNodeDescriptor = self.oIOXml.GetValueOfNodeAttribute(xImages[i], 'descriptor')
-            sImageType = self.oIOXml.GetValueOfNodeAttribute(xImages[i], 'type')
-            sImageDestination = self.oIOXml.GetValueOfNodeAttribute(xImages[i], 'destination')
-            sOrientation = self.oIOXml.GetValueOfNodeAttribute(xImages[i], 'orientation')
+            sVolumeFormat = self.oIOXml.GetValueOfNodeAttribute(xImages[indImage], 'format')
+            sNodeDescriptor = self.oIOXml.GetValueOfNodeAttribute(xImages[indImage], 'descriptor')
+            sImageType = self.oIOXml.GetValueOfNodeAttribute(xImages[indImage], 'type')
+            sImageDestination = self.oIOXml.GetValueOfNodeAttribute(xImages[indImage], 'destination')
+            sOrientation = self.oIOXml.GetValueOfNodeAttribute(xImages[indImage], 'orientation')
             sNodeName = self.sPageName + '_' + self.sPageDescriptor + '_' + sNodeDescriptor
 
             
-            # Extract type of image being loaded
-
-            xImageTypeNodes = self.oIOXml.GetChildren(xImages[i], 'Type')
-            
-            if len(xImageTypeNodes) > 1:
-                sWarningMsg = 'There can only be one type of image - using first definition'
-                self.oUtils.DisplayWarning(sWarningMsg) 
+#             # Extract type of image being loaded
+# 
+#             xImageTypeNodes = self.oIOXml.GetChildren(xImages[indImage], 'Type')
+#             
+#             if len(xImageTypeNodes) > 1:
+#                 sWarningMsg = 'There can only be one type of image.'
+#                 sWarningMsg = sWarningMsg + ' \nThe first image type definition will be used.'
+#                 sWarningMsg = sWarningMsg + '\nPage name: ' + self.sPageName + 'Page description: ' + self.sPageDescriptor
+#                 self.oUtils.DisplayWarning(sWarningMsg) 
                 
 #            self.sImageType = self.oIOXml.GetDataInNode(xImageTypeNodes[0])
             
 
             # Extract path element
-            
-            xPathNodes = self.oIOXml.GetChildren(xImages[i], 'Path')
+            xPathNodes = self.oIOXml.GetChildren(xImages[indImage], 'Path')
 
             if len(xPathNodes) > 1:
-                sWarningMsg = 'There can only be one path per image - using first defined path'
+                sWarningMsg = 'There can only be one path per image.  The first defined path will be used.'
+                sWarningMsg = sWarningMsg + '    Page name: ' + self.sPageName + '   Page description: ' + self.sPageDescriptor
                 self.oUtils.DisplayWarning( sWarningMsg )
 
             sImagePath = self.oIOXml.GetDataInNode(xPathNodes[0])
             
 
             # Extract destination layer (foreground, background, label)
-
-            xLayerNodes = self.oIOXml.GetChildren(xImages[i], 'Layer')
+            xLayerNodes = self.oIOXml.GetChildren(xImages[indImage], 'Layer')
             if len(xLayerNodes) > 1:
-                sWarningMsg = 'There can only be one layer per image - using first defined layer'
+                sWarningMsg = 'There can only be one destination layer (foreground, background or label) per image. \nThe first defined destination in the XML will be used.'
+                sWarningMsg = sWarningMsg + '    Page name: ' + self.sPageName + '   Page description: ' + self.sPageDescriptor
                 self.oUtils.DisplayWarning(sWarningMsg)
 
             sViewLayer = self.oIOXml.GetDataInNode(xLayerNodes[0])
+
+
+            # if image format/type is dicom/RTStruct, get the ROI elements
             
+            # set defaults to no RTSTruct
+            sSeriesInstanceUID = ''
+            sRoiVisibilityCode = 'Empty'
+            lsRoiList = []
+            if (sVolumeFormat == 'dicom' and sImageType == 'RTStruct'):
+                # extract Series UID nodes
+                xSeriesUIDNodes = self.oIOXml.GetChildren(xImages[indImage], 'SeriesInstanceUID')
+                if len(xSeriesUIDNodes) > 1:
+                    sWarningMsg = 'There can only be one SeriesInstanceUID element per image. \nThe first defined Series UID in the XML will be used.'
+                    sWarningMsg = sWarningMsg + '    Page name: ' + self.sPageName + '   Page description: ' + self.sPageDescriptor
+                    self.oUtils.DisplayWarning(sWarningMsg)
+
+                sSeriesInstanceUID = self.oIOXml.GetDataInNode(xSeriesUIDNodes[0])
+                sRoiVisibilityCode, lsRoiList = self.ReadROIElements(xImages[indImage])
             
             # Load images 
-            
             bLoadSuccess = True
             if (sVolumeFormat == 'dicom'):
-                bLoadSuccess, slNode = self.LoadDicomVolume(sImagePath, sImageType)                
+                bLoadSuccess, slNode = self.LoadDicomVolume(sImagePath, sImageType)   
 
             elif (sVolumeFormat in self.lValidVolumeFormats):
                 bLoadSuccess, slNode = self.LoadDataVolume(sNodeName, sImageType, sImagePath)
@@ -127,13 +147,97 @@ class ImageView:
                 bLoadSuccess = False
 
             if bLoadSuccess and (slNode is not None):
-                tupViewNode = [slNode, sImageDestination, sOrientation, sViewLayer]
+                tupViewNode =\
+                 [slNode, sImageDestination, sOrientation, sViewLayer,\
+                 sImageType, sSeriesInstanceUID, sRoiVisibilityCode, lsRoiList]
                 self.ltupViewNodes.append(tupViewNode)
     
         return self.ltupViewNodes
                     
                     
          
+    #-----------------------------------------------
+
+    def ReadROIElements(self, xImage):
+        # if the Image type is RTStuct, XML holds a visibility code and (if applicable)
+        # a list of ROI names
+        # The visibility code is as follows: 
+        #    'All' : turn on visibility of all ROIs in RTStruct
+        #    'None': turn off visibility of all ROIs in RTStruct
+        #    'Ignore' : turn on all ROIs except the ones listed
+        #    'Select' : turn on visibility of only ROIs listed
+        
+        # default - remains empty for All or None
+        lsRoiList = []
+        
+        # get XML ROIs element
+        xRoisNode = self.oIOXml.GetChildren(xImage, 'ROIs')
+        
+        # get visibility code from the attribute
+        sRoiVisibilityCode = self.oIOXml.GetValueOfNodeAttribute(xRoisNode[0], 'roiVisibilityCode')
+
+        if (sRoiVisibilityCode == 'Select' or sRoiVisibilityCode == 'Ignore'):
+            
+            # get list of ROI children
+            xRoiChildren = self.oIOXml.GetChildren(xRoisNode[0], 'ROI')
+
+            for indRoi in range(len(xRoiChildren)):
+                sRoiName = self.oIOXml.GetDataInNode(xRoiChildren[indRoi])
+                lsRoiList.append(sRoiName)
+                
+                print('**** ROI :%s' % sRoiName)
+        
+        return sRoiVisibilityCode, lsRoiList
+        
+    #-----------------------------------------------
+
+    def SetRoiVisibility(self, sSeriesInstanceUID, sRoiVisibilityCode, lsUserSelectRoiList):
+        
+        # in order to set visibility, get Slicer's subject hierarchy node (SHNode)
+        slSHNode = slicer.vtkMRMLSubjectHierarchyNode.GetSubjectHierarchyNode(slicer.mrmlScene)
+        
+        # get the item ID for the RTStruct through the Series Instance UID
+        slRTStructItemId = slSHNode.GetItemByUID(slicer.vtkMRMLSubjectHierarchyConstants.GetDICOMUIDName(), sSeriesInstanceUID)
+
+        # using slicers vtk Item Id for the RTStruct, get the ROI names (children)
+        slRTStructChildren = vtk.vtkIdList()    # initialize to ItemId type
+        slSHNode.GetItemChildren(slRTStructItemId, slRTStructChildren) # populate children variable
+        
+        # get ROI child Item ID and store the child (ROI) name
+        lsSubjectHierarchyROINames = []
+        for indROI in range(slRTStructChildren.GetNumberOfIds()):
+            slROIItemId = slRTStructChildren.GetId(indROI)
+            sROIName = slSHNode.GetItemName(slROIItemId)
+            lsSubjectHierarchyROINames.append(sROIName)
+        
+        # get segmentation node name from data node
+        slSegDataNode = slSHNode.GetItemDataNode(slRTStructItemId)
+        slSegNodeId = slSegDataNode.GetDisplayNodeID()
+        slSegNode = slicer.mrmlScene.GetNodeByID(slSegNodeId)
+        
+        
+        
+        if (sRoiVisibilityCode == 'All'):
+            for indSHList in range(len(lsSubjectHierarchyROINames)):
+                slSegNode.SetSegmentVisibility(lsSubjectHierarchyROINames[indSHList],True)
+        if (sRoiVisibilityCode == 'None'):
+            for indSHList in range(len(lsSubjectHierarchyROINames)):
+                slSegNode.SetSegmentVisibility(lsSubjectHierarchyROINames[indSHList],False)
+            
+        # turn ON all ROI's and then turn OFF user's list    
+        if (sRoiVisibilityCode == 'Ignore'):
+            for indSHList in range(len(lsSubjectHierarchyROINames)):
+                slSegNode.SetSegmentVisibility(lsSubjectHierarchyROINames[indSHList],True)
+            for indUserList in range(len(lsUserSelectRoiList)):
+                slSegNode.SetSegmentVisibility(lsUserSelectRoiList[indUserList], False)
+
+        # turn OFF all ROI's and then turn ON user's list    
+        if (sRoiVisibilityCode == 'Select'):
+            for indSHList in range(len(lsSubjectHierarchyROINames)):
+                slSegNode.SetSegmentVisibility(lsSubjectHierarchyROINames[indSHList],False)
+            for indUserList in range(len(lsUserSelectRoiList)):
+                slSegNode.SetSegmentVisibility(lsUserSelectRoiList[indUserList], True)
+        
     #-----------------------------------------------
 
     def LoadDataVolume(self, sNodeName, sImageType, sImagePath):
@@ -204,19 +308,20 @@ class ImageView:
             sExpectedSubjectHierarchyName = sPatientName + ' (' + sPatientID + ')'
 #             print(' ~~~ Subject Hierarchy expected name : %s' % sExpectedSubjectHierarchyName)
             
+            # get directory that stores the dicom series
             sHead_Tail = os.path.split(sDicomFilePath)
             sDicomSeriesDir = sHead_Tail[0]
             
-            
+            # check if already loaded into the database
             for sImportedSeries in lAllSeriesUIDs:
                 if sImportedSeries == sSeriesUIDToLoad:
                     bSeriesFoundInDB = True
                 
-            if not bSeriesFoundInDB:  # import all series in user specified directory
+            # if not already loaded, import from series directory    
+            if not bSeriesFoundInDB:
                 DICOMUtils.importDicom(sDicomSeriesDir)
 
 
-            
             # check subject hierarchy to see if volume already exists
             bVolumeAlreadyLoaded = False
             
