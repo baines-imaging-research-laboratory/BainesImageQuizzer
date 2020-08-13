0734c94af34c231dfddc9757000192c18a303ae8
Carol Johnson <cjohns83@uwo.ca>
2020-08-12 21:34:51 -0400

    Cleanup

--- ImageQuizzer/Utilities.py
+++ ImageQuizzer/Utilities.py
@@ -380,23 +380,23 @@ class UtilsMsgs:
     #-------------------------------------------
 
     def DisplayError(self,sErrorMsg):
-        self.msgBox.critical(slicer.util.mainWindow(),"ERROR",sErrorMsg)
+        self.msgBox.critical(slicer.util.mainWindow(),"Image Quizzer: ERROR",sErrorMsg)
         exit()
 
     #-------------------------------------------
 
     def DisplayWarning(self,sWarningMsg):
-        self.msgBox.warning(slicer.util.mainWindow(), 'Warning', sWarningMsg)
+        self.msgBox.warning(slicer.util.mainWindow(), 'Image Quizzer: Warning', sWarningMsg)
 
     #-------------------------------------------
 
     def DisplayInfo(self, sTextMsg):
-        self.msgBox.information(slicer.util.mainWindow(), 'Information', sTextMsg)
+        self.msgBox.information(slicer.util.mainWindow(), 'Image Quizzer: Information', sTextMsg)
 
     #-------------------------------------------
     
     def DisplayYesNo(self, sMsg):
-        qtAns = self.msgBox.question(slicer.util.mainWindow(),'Continue?',sMsg, qt.QMessageBox.Yes, qt.QMessageBox.No)
+        qtAns = self.msgBox.question(slicer.util.mainWindow(),'Image Quizzer: Continue?',sMsg, qt.QMessageBox.Yes, qt.QMessageBox.No)
         return qtAns
 
 ##########################################################################
